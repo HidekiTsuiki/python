592a41875268b7c2fba6857706cef372086980f3
Hideki Tsuiki <tsuiki@i.h.kyoto-u.ac.jp>
2022-10-18 17:23:33 +0900

    tsuiki

---
+++ factorial.py
@@ -0,0 +1,12 @@
+''' 階乗関数のモジュール'''
+a = 1
+def fact(n):
+    '''n の階乗を計算する'''
+    if(n == 1):
+        return a
+    return n*fact(n-1)
+
+fact100 = fact(100)
+print(fact(10))
+if (__name__ == "__main__"):
+    print(fact(int(input("数を入力してください:"))))

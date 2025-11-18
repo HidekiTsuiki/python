1c58984bb3fc214b1e27b0dd73cab560f5580b64
Hideki Tsuiki <tsuiki@i.h.kyoto-u.ac.jp>
2025-11-18 10:04:08 +0900

    tsuiki

--- factorial.py
+++ factorial.py
@@ -5,10 +5,10 @@ def fact(n):
     '''n の階乗を計算する'''
     if(n == 1):
         return a
-    print("name is", __name__)
+#    print("name is", __name__)
     return n*fact(n-1)
 
 fact100 = fact(100)
 print(fact(10))
-if (__name__ == "__main__"):
-    print(fact(int(input("数を入力してください:"))))
+# if (__name__ == "__main__"):
+#     print(fact(int(input("数を入力してください:"))))
